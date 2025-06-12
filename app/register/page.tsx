f59164c97f0d0f9d7e4f799ecd91dd972809b4b1
dhanielcodes <me@dhaniel.site>
2025-06-12 14:10:35 +0100

    feat: refactor registration flow by consolidating page components, enhancing state management, and integrating SEO for improved user experience

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,16 +4,25 @@ import { Form, Formik } from "formik";
 import { Onboard } from "./Onboard";
 import Box from "@/components/bits/Box";
 import Stepper from "@/components/Stepper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { stepOneSchema, stepThreeSchema, stepTwoSchema } from "./validation";
 import useSignUp from "@/hooks/signUp";
+import { useParams } from "next/navigation";
+import BconSeo from "@/components/BconSeo";
 
 export default function Page() {
   const [active, setActive] = useState<number>(1);
-  const { mutate, isPending } = useSignUp();
+  const { mutate, isPending, data } = useSignUp();
+  const params = useParams();
 
+  useEffect(() => {
+    setActive(1);
+  }, [data]);
+
+  const id = (params?.id as string)?.toLowerCase() || null;
   return (
     <div>
+      <BconSeo title={`Register Under Marketer - ${id}`} />
       <Box className="rounded-t-none">
         <div className="grid place-items-center">
           <Stepper steps={3} active={active} setActive={setActive} />
@@ -49,7 +58,7 @@ export default function Page() {
           //"companyName": "WellFed",
           onboardingSource: "Web",
           //"deviceId": "Tets",
-          agentId: "",
+          agentId: id,
           code: "",
         }}
         validationSchema={
@@ -62,11 +71,11 @@ export default function Page() {
         onSubmit={(values) => {
           console.log(values, "values");
           if (active === 3) {
-            const { code, agentId,accountType,...restValues } = values;
+            const { code, agentId, accountType, ...restValues } = values;
             const payload = {
               ...restValues,
-              accountType:Number(accountType),
-              agentId:agentId ? Number(agentId):0,
+              accountType: Number(accountType),
+              agentId: agentId ? Number(agentId) : 0,
               phone:
                 values?.phone?.length === 11
                   ? values?.code + values?.phone?.slice(1, 11)
